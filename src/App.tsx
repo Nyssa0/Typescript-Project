85d619566b67d14093040251147a804acddbbf6d
Corentin Deleuse <dlse.corentin@gmail.com>
2024-05-15 15:02:09 +0200

    feat: remove useless title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   const { tasks } = useContext(ToDoContext);
   return (
     <div className="flex flex-col items-center justify-start p-6 gap-6 bg-gray-100 min-h-screen">
-      <h1 className="font-bold text-3xl text-gray-800">ToDo App</h1>
       <div className="w-full">
         <ToDoList tasks={tasks} />
       </div>
