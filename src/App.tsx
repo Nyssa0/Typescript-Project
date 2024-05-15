56c2ae847847afe20788ca08dab847578a5aa938
IDRAYNAR <rayan.idri@edu.devinci.fr>
2024-05-15 14:37:43 +0200

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import ToDoManager from './components/ToDoManager';
 function App() {
   return (
     <div className="flex flex-col items-center justify-start p-6 gap-6 bg-gray-100 min-h-screen">
-      <h1 className="font-bold text-3xl text-gray-800">ToDo App</h1>
       <ToDoManager />
     </div>
   );
